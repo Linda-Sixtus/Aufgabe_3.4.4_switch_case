37d0702c171c693d89113b7bfce5820dfef83a5a
Linda Sixtus <linda.sixtus@gmx.de>
2025-06-12 11:52:55 +0200

    Aufgabe 1

---
+++ main.dart
@@ -0,0 +1,37 @@
+enum Weekday {
+monday,
+tuesday,
+wednesday,
+thursday,
+friday,
+saturday,
+sunday,
+}
+
+void main(){
+
+Weekday? weekday;
+
+weekday=null;
+
+switch (weekday){
+
+  case Weekday.monday:
+  case Weekday.tuesday:
+    print (
+      "Montage und Dienstage sind anstrengend ^^."
+      );
+  case Weekday.wednesday:
+  case Weekday.thursday:
+    print (
+      "Mittwoch und Donnerstag sind OK XD."
+    );
+  case Weekday.friday:
+    print ("Freitag ist Superduper.OwO");
+  case Weekday.saturday:
+    print("Wochenende ist genial :D..");
+  default: 
+    print("Kein Tag ausgewählt.");
+}
+
+}
